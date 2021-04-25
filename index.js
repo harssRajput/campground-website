dd2ff589d5009032af01eec4b0b6f6695d5918da
Harsh Rajput <hrrajput20@gmail.com>
2021-04-25 22:04:40 +0530

    set specific allow urls for content-security-policy middleware of helmet

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
@@ -91,6 +92,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
+                "https://source.unsplash.com/",
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
